bd99763867c14b4a0e817ba796c0234b125559d4
kwanghoon.kim <kk03187693@gmail.com>
2020-12-22 23:56:17 +0900

    add order entity logic

--- src/main/java/com/kwanghoon/jpashop/domain/Order.java
+++ src/main/java/com/kwanghoon/jpashop/domain/Order.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static javax.persistence.CascadeType.*;
@@ -50,5 +51,38 @@ public class Order {
     public void setDelivery(Delivery delivery) {
         this.delivery = delivery;
         delivery.setOrder(this);
+    }
+
+    /* 생성 메서드 */
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+
+        for (OrderItem orderItem: orderItems) {
+            order.addOrderItem(orderItem);
+        }
+
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+
+        return order;
+    }
+
+    /* 비즈니스 로직 */
+    public void cancel() {
+        if (delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송완료돈 상품은 취소가 불가능합니다.");
+        }
+
+        this.setStatus(OrderStatus.CANCEL);
+        orderItems.forEach(OrderItem::canccel);
+    }
+
+    public int getTotalPrice() {
+        return orderItems
+            .stream()
+            .mapToInt(OrderItem::getTotalPrice)
+            .sum();
     }
 }
