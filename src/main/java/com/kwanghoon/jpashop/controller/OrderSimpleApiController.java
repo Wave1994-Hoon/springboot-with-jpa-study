7b839cd59e0a7a467ce110a3bc810ec1da315cd5
kwanghoon.kim <kk03187693@gmail.com>
2020-12-31 21:48:18 +0900

    add v2 order simple api

--- src/main/java/com/kwanghoon/jpashop/controller/OrderSimpleApiController.java
+++ src/main/java/com/kwanghoon/jpashop/controller/OrderSimpleApiController.java
@@ -1,13 +1,18 @@
 package com.kwanghoon.jpashop.controller;
 
+import com.kwanghoon.jpashop.domain.Address;
 import com.kwanghoon.jpashop.domain.Order;
 import com.kwanghoon.jpashop.domain.OrderSearch;
+import com.kwanghoon.jpashop.domain.OrderStatus;
 import com.kwanghoon.jpashop.repository.OrderRepository;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /*
 * XToOne(ManyToOne, OneToOne)
@@ -23,6 +28,10 @@ public class OrderSimpleApiController {
 
 
     /*
+    * V1
+    * - 엔티티 직접 반환
+    *
+    * 문제점
     * 서로 toString method 호출하여 무한 루프 발생
     * 양방향 관계 문제 발생 -> @JsonIgnore
     * Hibernate5Module 모듈 등록, LAZY=null 처리
@@ -39,5 +48,42 @@ public class OrderSimpleApiController {
         });
 
         return all;
+    }
+
+    /*
+    * V2
+    * - 엔티티 직접 반환 대신 DTO 반환
+    *
+    * 문제점
+    * 지연로딩으로 너무 많은 SQL Query 발생 (N+1)
+    *
+    * 참고
+    * 지연 로딩은 영속성 컨텍스트에 있으면 영속성 컨텍스트에 있는 엔티티를 사용하고 없으면 SQL을 실행한다.
+    * 따라서 같은 영속성 컨텍스트에 이미 로딩한 회원 엔티티를 추가로 조회하면 SQL을 실행하지 않는다.
+    */
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> orderV2() {
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+
+        return orders.stream()
+            .map(SimpleOrderDto::new)
+            .collect(Collectors.toList());
+    }
+
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate(); // Lazy 초기화
+            orderStatus = order.getStatus();
+            address = order.getMember().getAddress(); // Lazy 초기화
+        }
     }
 }
